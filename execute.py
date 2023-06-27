a18ea38abf7d85e13395b60ff183b8e6817093f7
Hoàng Tuấn Huy <kubin2712@gmail.com>
2023-06-28 02:34:40 +0700

    update base

---
+++ execute.py
@@ -0,0 +1,19 @@
+﻿import subprocess
+
+def run_build_client():
+    subprocess.run(
+        "npm run build",
+        shell=True,
+        cwd='./client_side'
+    )
+    
+def run_server():
+    subprocess.run(
+        "python main.py",
+        shell=True,
+        cwd='./server_side'
+    )
+
+if __name__ == '__main__':
+    run_build_client()
+    run_server()
---
+++ server_side/main.py
@@ -0,0 +1,37 @@
+﻿from flask import Flask, render_template
+import os
+import subprocess
+import dotenv
+
+### Load .env file
+dotenv.load_dotenv(dotenv_path='../.env')
+ADDRESS = os.getenv("ADDRESS")
+PORT = os.getenv("PORT")
+TEMPLATE_PATH = os.getenv("TEMPLATE_PATH")
+STATIC_PATH = os.getenv("STATIC_PATH")
+### End load .env file
+
+app = Flask(__name__, template_folder=TEMPLATE_PATH, static_folder=STATIC_PATH)
+
+
+@app.after_request
+def before_request(response):
+    headers = {
+        'Cache-Control': 'public, max-age=3600'
+    }
+    for header, value in headers.items():
+        response.headers[header] = value
+    
+    return response
+
+@app.route('/', methods=['GET'])
+def process_root():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(
+        host=ADDRESS,
+        port=PORT,
+        debug=True
+    )
