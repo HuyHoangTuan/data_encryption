6f239deec467a8c334cf905ce46eeb2e5ef792de
HuyHt <kubin2712@gmail.com>
2023-06-28 20:16:39 +0700

    update

--- client_side/configs/webpack/common.js
+++ client_side/configs/webpack/common.js
@@ -44,6 +44,10 @@ module.exports = {
                 exclude: /node_modules/,
                 use: ["babel-loader"],
             },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
         ],
     },
     plugins: [
